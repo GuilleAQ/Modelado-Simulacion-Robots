283c86c68827b0da791f4b62651cedd66b6f38c5
GuilleAQ <guillermoalque8@gmail.com>
2025-04-02 19:59:17 +0200

    chassis & wheels

--- pybullet/P2/test.py
+++ pybullet/P2/test.py
@@ -23,8 +23,20 @@ for j in range (numJoints):
     print("%d - %s" % (p.getJointInfo(robotId,j)[0], p.getJointInfo(robotId,j)[1].decode("utf-8")))
     print("Link - %s" % (p.getJointInfo(robotId,j)[12]))
 
+wheel_joints = [3, 7, 11, 15]
+wheel_links_vel = p.addUserDebugParameter("wheel_links_vel", -10, 10, 0)
 
 input("Press enter to start motion")
 while True:
-    p.setJointMotorControlArray(robotId, joints, p.VELOCITY_CONTROL, targetVelocities=[11] * 2)
+    # p.setJointMotorControlArray(robotId, joints, p.VELOCITY_CONTROL, targetVelocities=[11] * 2)
 
+    tl_vel = p.readUserDebugParameter(wheel_links_vel)
+    p.setJointMotorControl2(robotId, wheel_joints[0], p.VELOCITY_CONTROL, targetVelocity=tl_vel)
+    p.setJointMotorControl2(robotId, wheel_joints[1], p.VELOCITY_CONTROL, targetVelocity=tl_vel)
+    p.setJointMotorControl2(robotId, wheel_joints[2], p.VELOCITY_CONTROL, targetVelocity=-tl_vel)
+    p.setJointMotorControl2(robotId, wheel_joints[3], p.VELOCITY_CONTROL, targetVelocity=-tl_vel)
+    # p.setJointMotorControlArray(robotId, wheel_joints, p.VELOCITY_CONTROL, targetVelocities=[tl_vel] * 4)
+    # p.setJointMotorControl2(robotId, wheel_joints, p.VELOCITY_CONTROL, targetVelocity=tl_vel)
+
+    # p.stepSimulation()
+    time.sleep(1./240.)
