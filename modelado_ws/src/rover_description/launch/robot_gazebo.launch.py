7ab1de7b8c9b02230f975508b9ab707c16cac0ad
GuilleAQ <guillermoalque8@gmail.com>
2025-05-18 14:50:15 +0200

    in progress

--- modelado_ws/src/rover_description/launch/robot_gazebo.launch.py
+++ modelado_ws/src/rover_description/launch/robot_gazebo.launch.py
@@ -11,81 +11,96 @@ from launch_ros.substitutions import FindPackageShare
 from moveit_configs_utils import MoveItConfigsBuilder
 
 # Function to start the Gazebo server and client
-def start_gzserver(context, *args, **kwargs):  
-    # TBD: Define pkg_path
-    # world_name = 'small_house'
+def start_gzserver(context, *args, **kwargs):
+    pkg_path = get_package_share_directory('urjc_excavation_world')
     world_name = LaunchConfiguration('world_name').perform(context)
-    # TBD: Define world path
+    world = join(pkg_path, 'worlds', world_name + '.world')
+    # world = 'empty.sdf'
 
     # Launch Gazebo server
     start_gazebo_server_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            join(get_package_share_directory('ros_gz_sim'), 'launch',
-                         'gz_sim.launch.py')),
-        launch_arguments={'gz_args': ['-r -s -v 4 ', world]}.items()
+            join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': f'-r -s -v 4 {world}'}.items()
     )
 
-    # Launch Gazebo client
+    # Launch Gazebo Client
     start_gazebo_client_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            join(get_package_share_directory('ros_gz_sim'),
-                         'launch',
-                         'gz_sim.launch.py')
+            join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
         ),
-        launch_arguments={'gz_args': [' -g ']}.items(),
+        launch_arguments={'gz_args': ' -g '}.items()
     )
 
     return [start_gazebo_server_cmd, start_gazebo_client_cmd]
 
 # Function to get the model paths
 def get_model_paths(packages_names):
-    model_paths = ""
+    model_paths = ''
     for package_name in packages_names:
-        if model_paths != "":
+        if model_paths:
             model_paths += pathsep
-
         package_path = get_package_prefix(package_name)
-        model_path = join(package_path, "share")
-
+        model_path = join(package_path, 'share')
         model_paths += model_path
 
     if 'GZ_SIM_RESOURCE_PATH' in environ:
         model_paths += pathsep + environ['GZ_SIM_RESOURCE_PATH']
-
     return model_paths
 
 # Launch description
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("robot", package_name="TBD").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder('rover', package_name='rover_moveit_config').to_moveit_configs()
     declare_sim_time = DeclareLaunchArgument(
-        'use_sim_time', default_value='true',
-        description="use_sim_time simulation parameter"
+        'use_sim_time', default_value='true', description='use_sim_time simulation parameter'
     )
     model_path = ''
-    resource_path = ''
-
-    pkg_path = get_package_share_directory('robot_description')
-    model_path += join(pkg_path, 'models')
-    resource_path += pkg_path + model_path
-
+    pkg_path = get_package_share_directory('rover_description')
+    model_path = join(pkg_path, 'models')
+    resource_path = model_path
     if 'GZ_SIM_MODEL_PATH' in environ:
-        model_path += pathsep+environ['GZ_SIM_MODEL_PATH']
-    if 'GZ_SIM_RESOURCE_PATH' in environ:
-        resource_path += pathsep+environ['GZ_SIM_RESOURCE_PATH']
+        model_path += pathsep + environ['GZ_SIM_MODEL_PATH']
+        resource_path += pathsep + environ['GZ_SIM_RESOURCE_PATH']
 
-    model_path = get_model_paths(['robot_description'])
+    model_path = get_model_paths(['rover_description'])
 
     robot_description_launcher = IncludeLaunchDescription(
-       PathJoinSubstitution(
-           [FindPackageShare("robot_moveit_config"), "launch", "rsp.launch.py"]
-       ),
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [FindPackageShare('rover_moveit_config'), 'launch', 'rsp.launch.py']
+            )
+        )
     )
 
     start_gazebo_server_cmd = OpaqueFunction(function=start_gzserver)
 
-    # TBD: RViz config and launching
-    # TBD: robot spawner 
+    # RViz config and launching
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare('rover_description'), 'rviz', 'robot.rviz']
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='log',
+        arguments=['-d', rviz_config_file],
+    )
 
+    # Spawning robot
+    gazebo_spawn_robot = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=[
+            '-model',
+            'rover',
+            '-topic',
+            'robot_description',
+            '-use_sim_time',
+            'True',
+        ],
+    )
 
     # Bridge
     bridge = Node(
@@ -95,7 +110,7 @@ def generate_launch_description():
         parameters=[
             {
                 'config_file': join(
-                    pkg_path, 'config', 'robot_bridge.yaml'
+                    pkg_path, 'config', 'rover_bridge.yaml'
                 ),
                 'use_sim_time': True,
             }
@@ -141,7 +156,7 @@ def generate_launch_description():
     ld.add_action(bridge)
     ld.add_action(gz_image_bridge_node)
     ld.add_action(start_gazebo_server_cmd)
-    # TBD: RViz config and launching
-    # TBD: robot spawner 
+    ld.add_action(rviz_node)
+    ld.add_action(gazebo_spawn_robot)
     ld.add_action(twist_stamped)
     return ld
